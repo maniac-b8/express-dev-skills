d145f70db3f1a8abf4aa2ea4e9efb5c14ea75df9
maniac-b8 <matthewdixon2539@gmail.com>
2024-04-10 10:04:51 -0700

    edit router names

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/skills');
+const skillsRouter = require('./routes/skills');
 
 const app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/skills', usersRouter);
+app.use('/skills', skillsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
